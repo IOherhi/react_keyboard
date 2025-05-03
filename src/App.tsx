e0c855f073b5c0b4b808e6133860eb17c4f44085
Ivan Herhi <ionherhi@gmail.com>
2025-05-03 20:11:17 +0200

    verion01

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+export class App extends React.Component {
+  state = {
+    lastKey: null,
+  };
+
+  handleKeyDown = (event: React.KeyboardEvent): void => {
+    this.setState({ lastKey: event.key });
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    const { lastKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {lastKey === null
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${lastKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
